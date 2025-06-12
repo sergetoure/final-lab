3b62b4122fa855062fbd4fd6cbe0344f2469d69d
Serge Toure <26191503+sergetoure09@users.noreply.github.com>
2025-06-12 22:34:52 +0200

    modal ok

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,16 @@ import Header from "../components/header";
 import LandingSection from "../components/LandingSection.jsx";
 import ProjectSection from "../components/ProjectSection.jsx";
 import ContactMe from "../components/ContactMe.jsx";
+import Modal from "../components/Modal.jsx";
+import { useState } from "react";
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleModal = () => setIsOpen(false);
+  const showModal = (e) => {
+    e.preventDefault();
+    // Simulate form submission
+    setIsOpen(true);
+  };
   return (
     <>
       <Header />
@@ -12,9 +21,13 @@ function App() {
 
         <ProjectSection />
 
-        <ContactMe />
+        <ContactMe onShowModal={showModal} />
       </main>
       <footer className="App-footer">hello footer</footer>
+      <Modal isOpen={isOpen} onClose={toggleModal}>
+        <h2>All good!</h2>
+        <p>Thanks for your submission Serge, we will get back to you shortly</p>
+      </Modal>
     </>
   );
 }
